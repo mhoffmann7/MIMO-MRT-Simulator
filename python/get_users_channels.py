553e79c7cd9c092dd693f651db97c1e914f4bc30
mhoffmann7 <marcin.r.hoffmann@gmail.com>
2022-03-26 18:06:15 +0100

    added file for loading radio channel and user association

---
+++ python/get_users_channels.py
@@ -0,0 +1,6 @@
+"""
+This file is responsible for loading radio channels
+from files and obtaining random users. The channel is
+pregenerated for N users function can select random 
+topology of K users.
+"""
---
+++ python/user_association.py
@@ -0,0 +1,7 @@
+"""
+File that will contain procedure of user to BSs
+association, three algorithms will be supported:
+- RX    - based on received signal strength
+- NO_UE - aimed at balancing the load between cells
+- SE    - aimed at spectral efficiency maximization 
+"""
