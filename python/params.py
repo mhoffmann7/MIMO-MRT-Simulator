3e6da621c1057fe0b25c5c2096f1418471fe624a
mhoffmann7 <marcin.r.hoffmann@gmail.com>
2022-03-28 19:32:11 +0200

    Added user assignment by received signal strtength

--- python/params.py
+++ python/params.py
@@ -2,16 +2,17 @@
 File containing simulation parameters, to be done is to replace it
 with loading parameters from json file.
 """
+import numpy as np
 
 # minimum received signal strength (RSS) necessary to serve user
 min_rec_power_dbm = -120 # dBm
 
 # number of antennas, channels were generated for 1 Macro BS of 128 antennas
 # and 5 Pico BSs of 32 antennas 
-antennas = [128, 32, 32, 32, 32, 32]
+antennas = np.array([128, 32, 32, 32, 32, 32])
 
 # TX power
-tx_power_dbm = [46, 30, 30, 30, 30, 30] # dBm
+tx_power_dbm = np.array([46, 30, 30, 30, 30, 30]) # dBm
 
 # Thermal noise power
 noise_pwr_dbm = -174 # dBm/Hz
--- python/user_association.py
+++ python/user_association.py
@@ -5,3 +5,22 @@ association, three algorithms will be supported:
 - NO_UE - aimed at balancing the load between cells
 - SE    - aimed at spectral efficiency maximization 
 """
+import numpy as np
+import params
+
+def userAssociation(method,pathloss):
+    no_bs, no_ue = pathloss.shape
+    rss = np.transpose(pathloss) + np.tile(params.tx_power_dbm, (no_ue, 1))
+    
+    if method == 'RX':
+        assignment = RX(rss, no_ue)
+
+    return assignment
+
+
+def RX(rss, no_ue):
+    assignment = np.zeros((no_ue))
+    max_rss = np.max(rss,axis=1)
+    assignment[max_rss > params.min_rec_power_dbm] =\
+         np.argmax(rss[max_rss > params.min_rec_power_dbm,:],axis=1)+1
+    return assignment
